3599cfa8fc7654e8c6484189e0688c96668e57f8
Your Name <you@example.com>
2021-12-20 23:56:03 +0300

    vfinal

--- src/App.js
+++ src/App.js
@@ -1,25 +1,60 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from "react";
+import { BrowserRouter } from "react-router-dom";
+import { ToastContainer } from "react-toastify";
+import { getUser } from "./api/user-service";
+import { getVehicles } from "./api/vehicle-service";
+import Footer from "./components/common/Footer";
+import MenuBar from "./components/common/MenuBar";
+import TopBar from "./components/common/TopBar";
+import LoadingPage from "./pages/LoadingPage";
+import CustomRoutes from "./router/CustomRoutes";
+import { useStore } from "./store";
+import { loginSuccess } from "./store/user/userActions";
+import { setVehiclesInStore } from "./store/vehicles/vehiclesActions";
 
-function App() {
+const App = () => {
+  const [loading, setLoading] = useState(true);
+  const { dispatchUser, dispatchVehicles } = useStore();
+
+  const loadData = async () => {
+ 
+    try {
+
+      /**** LOAD VEHICLES ****/
+      const respVehicles = await getVehicles();
+      if(respVehicles.status !==200) throw "An error occured whlie getting vehicles";
+      dispatchVehicles(setVehiclesInStore(respVehicles.data));
+      
+
+
+      /**** LOAD USER ****/
+      const respUser = await getUser();
+      if (respUser.status !== 200) throw "An error occured whlie getting user";
+      dispatchUser(loginSuccess(respUser.data));
+
+      setLoading(false);
+
+    } catch (error) {
+      console.log(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    loadData();
+  }, []);
+
+  if(loading) return(<LoadingPage/>)
+  else
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+    <BrowserRouter>
+      <TopBar />
+      <MenuBar />
+      <CustomRoutes />
+      <Footer />
+      <ToastContainer />
+    </BrowserRouter>
+  )
+};
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import "bootstrap/dist/css/bootstrap.min.css";
+import "react-toastify/dist/ReactToastify.min.css";
+import "alertifyjs/build/css/alertify.min.css";
+
+import "./assets/css/style.css";
+import { StoreProvider } from './store';
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <StoreProvider>
+      <App />
+    </StoreProvider>
+
   </React.StrictMode>,
   document.getElementById('root')
 );
